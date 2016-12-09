bae96292ef27b185c1b7f42c19ba388d735dd555
Chewbacca96 <lebedev582@gmail.com>
2016-12-09 15:55:18 +0300

    Try

--- js/index.js
+++ js/index.js
@@ -7,3 +7,14 @@ app.Todo = Backbone.Model.extend({
    }
 });
 
+app.TodoList = Backbone.Collection.extend({
+    model: app.Todo,
+    url: "http://api.moto.site/item"
+});
+
+app.todoList = new app.TodoList;
+app.todoList.fetch();
+console.log(JSON.stringify(app.todoList));
+
+
+
